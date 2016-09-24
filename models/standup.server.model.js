65356e4af0e1e5f32711f2df0f9d20a4613d5de9
John Doe <webdevnorth@gmail.com>
2016-09-24 18:35:34 +0100

    included more notes on model construction, exported standup model (standup)

--- models/standup.server.model.js
+++ models/standup.server.model.js
@@ -10,6 +10,16 @@ var standupSchema = new Schema({
 	createdOn: { type: Date, default: Date.now }		
 });
 
+module.exports = mongoose.model('Standup', standupSchema);
+// first arg: name of model, second arg: schema from which to build model
+// third arg (optional): name of collection (defaults to pluralised version of first arg),
+// to which document instances of this model will be saved. this can also be used to match
+// to existing collections
+
+
+// FURTHER NOTES
+/////////////////////////////////////
+
 // disabled id
 	var noIdSchema = new Schema({ name: String }, { _id: false });
 
@@ -46,6 +56,15 @@ var standupSchema = new Schema({
 		});
 	}
 
-
 // remember, schema definitions are essentially just JSON, 
 // so you could also load in a saved JSON file as your schema
+
+// RE: Models
+// Any number of models can be constructed from one schema
+// You could even append additional key-value pairs to the schema,
+// customising it further. E.g.:
+
+// some existing schema
+// build a model from this schema
+// append new key-value pairs to the schema via .add({})
+// build a slightly different model from this updated schema
